861213a9223ad0629542e18da47f2e8980a1c67c
Cecília do Lago <ceciliadolago@gmail.com>
2025-11-25 19:07:15 -0300

    packages updates

---
+++ pacotes_utilizados.R
@@ -0,0 +1,27 @@
+
+
+
+# Lista de pacotes:
+
+install.packages("collapse")
+install.packages("concaveman")
+install.packages("devtools")
+install.packages("furrr")
+install.packages("future.apply")
+install.packages("geobr")
+install.packages("geos")
+install.packages("gitcreds")
+install.packages("mapview")
+install.packages("nngeo")
+install.packages("openxlsx")
+install.packages("piggyback")
+install.packages("polyclip")
+install.packages("progressr")
+install.packages("rgdal")
+install.packages("rgeos")
+install.packages("rmapshaper")
+install.packages("smoothr")
+install.packages("sp")
+install.packages("tictoc")
+install.packages("usethis")
+install.packages("xlsx")
