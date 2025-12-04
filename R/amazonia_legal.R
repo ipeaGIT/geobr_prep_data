cbbe47b61a87af962c3c77fd2eb3a38533651243
Cecília do Lago <ceciliadolago@gmail.com>
2025-12-04 14:45:39 -0300

    standarize formatting

--- R/amazonia_legal.R
+++ R/amazonia_legal.R
@@ -19,29 +19,29 @@
 # Informação do Sistema de Referência: SIRGAS 2000
 # Observações: 2014 a 2020 sem shape. 2021 a 2024 com shape.
 
-### Libraries (use any library as necessary) ----
+### Libraries (use any library as necessary) -----------------------------------
 
-library(arrow)
-library(geoarrow)
-library(stringr)
-library(sf)
-library(janitor)
-library(dplyr)
-library(readr)
-library(data.table)
-library(magrittr)
-library(devtools)
-library(lwgeom)
-library(stringi)
-library(tidyverse)
-library(rvest)
-source("./R/support_harmonize_geobr.R")
-source("./R/support_fun.R")
+# library(arrow)
+# library(geoarrow)
+# library(stringr)
+# library(sf)
+# library(janitor)
+# library(dplyr)
+# library(readr)
+# library(data.table)
+# library(magrittr)
+# library(devtools)
+# library(lwgeom)
+# library(stringi)
+# library(tidyverse)
+# library(rvest)
+# source("./R/support_harmonize_geobr.R")
+# source("./R/support_fun.R")
 
-# Download the data  ----
+# Download the data  -----------------------------------------------------------
 download_amazonialegal <- function(year){ #
 
-  ## 0. Set up the download links (UPDATE YEAR) ----
+  ## 0. Set up the download links (UPDATE YEAR) --------------------------------
   
   base_link <- "https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/amazonia_legal/"
   
@@ -63,7 +63,7 @@ download_amazonialegal <- function(year){ #
                       "_shp.zip")
   }
   
-  ## 1. Directions do download the file ----
+  ## 1. Directions do download the file ----------------------------------------
 
   zip_dir <- paste0(tempdir(), "/amazonia_legal/", year)
   dir.create(zip_dir, showWarnings = FALSE, recursive = TRUE)
@@ -78,7 +78,7 @@ download_amazonialegal <- function(year){ #
   dir.create(out_zip, showWarnings = FALSE, recursive = TRUE)
   dir.exists(out_zip)
  
-  ## 2. Download and save in the temp directory ----
+  ## 2. Download and save in the temp directory --------------------------------
   
   file_raw <- fs::file_temp(tmp_dir = in_zip,
                             ext = fs::path_ext(ftp_zip))
@@ -88,11 +88,11 @@ download_amazonialegal <- function(year){ #
   
   file.exists(file_raw)
   
-  ## 3. Unzip the shape file ----
+  ## 3. Unzip the shape file ---------------------------------------------------
   
   unzip_geobr(zip_dir = zip_dir, in_zip = in_zip, out_zip = out_zip, is_shp = TRUE)
   
-  ## 4. Read data ----
+  ## 4. Read data --------------------------------------------------------------
   
   if (year %in% c(2019, 2020)) {
     # lista todos os .shp na pasta (busca recursiva opcional)
@@ -113,30 +113,30 @@ download_amazonialegal <- function(year){ #
     )
   }
   
-  ## 5. Show result ----
+  ## 5. Show result ------------------------------------------------------------
   glimpse(amazonialegal_raw)
   
   return(amazonialegal_raw)
   
 }
   
-# Clean the data ----
+# Clean the data ---------------------------------------------------------------
 clean_amazonialegal <- function(amazonialegal_raw, year){
   
-  ## 0. create clean directory ----
+  ## 0. create clean directory -------------------------------------------------
   
   #create directory
   dir_clean <- paste0("./data/amazonia_legal/", year)
   dir.create(dir_clean, showWarnings = FALSE)
   dir.exists(dir_clean)
   
-  ## 1. rename column names ----
+  ## 1. rename column names ----------------------------------------------------
   
   # # Rename columns
   # amazonialegal_raw$GID0 <- NULL
   # amazonialegal_raw$ID1 <- NULL
   
-  ## 2. Apply geobr cleaning ----
+  ## 2. Apply geobr cleaning ---------------------------------------------------
   
   temp_sf <- harmonize_geobr(
     temp_sf = amazonialegal_raw,
@@ -153,20 +153,20 @@ clean_amazonialegal <- function(amazonialegal_raw, year){
 
   glimpse(temp_sf)
   
-  ## 3. generate a lighter version of the dataset with simplified borders ----
+  ## 3. generate a lighter version of the dataset with simplified borders ------
   
   # simplify
   temp_sf2 <- simplify_temp_sf(temp_sf)
   head(temp_sf2)
   
-  ## 4. Clean data set and save it in geopackage format ----
+  ## 4. Clean data set and save it in geopackage format ------------------------
   
   #save original and simplified datasets
   # sf::st_write(temp_sf, append = FALSE, dsn = paste0(dir_clean, "amazonialegal", ".gpkg") )
   # sf::st_write(temp_sf2, append = FALSE, dsn = paste0(dir_clean, "amazonialegal","_simplified", ".gpkg"))
   
   
-  ## 5. Save original and simplified datasets in parquet ----
+  ## 5. Save original and simplified datasets in parquet -----------------------
   arrow::write_parquet(
     x = temp_sf,
     sink = paste0(dir_clean, "/amazonialegal_", year, ".parquet"),
